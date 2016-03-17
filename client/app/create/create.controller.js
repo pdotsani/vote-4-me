9102fcee2e7b86126f54233633dc4828b37ac76a
Patrick San Juan <pat.sanjuan@gmail.com>
2016-03-17 11:18:11 -0700

    fixed poll model and binding issues

--- client/app/create/create.controller.js
+++ client/app/create/create.controller.js
@@ -2,31 +2,21 @@
 (function(){
 
 class CreateComponent {
-  constructor($http, toaster, poll) {
+  constructor($http, toaster, poll, Auth) {
   	this.$http = $http;
+    this.auth = Auth;
     this.toaster = toaster;
     this.poll = poll;
     this.question = '';
-    this.form = document.getElementById('create')
     this.answers = [{response:'', count:0},
       {response:'', count:0},
       {response:'', count:0}];
-  }
-
-  resetVars() {
-    this.answers = [{response:'', count:0},
-      {response:'', count:0},
-      {response:'', count:0}]; 
-    this.question = '';
-  }
-
-  resetFields() {
-  	this.resetVars();
-  	this.toaster
-  		.pop('info', 
-  			'Form Reset', 
-  			 this.answers.length + ' responses in cue...', 
-  			3000);
+    this.resetVars = function() {
+      this.answers = [{response:'', count:0},
+        {response:'', count:0},
+        {response:'', count:0}]; 
+      this.question = '';
+    };
   }
 
   addAnswer() {
@@ -41,37 +31,62 @@ class CreateComponent {
 				3000);
   }
 
-  createPoll() {
-    console.log('form', this.form);
-    console.log('question', this.question);
-    console.log('answers', this.answers);
-    // this.resetVars();
-    if(this.form.$valid) {
-      this.poll.create({},{
+  resetFields() {
+    this.resetVars();
+    this.toaster
+      .pop('info', 
+        'Form Reset', 
+         this.answers.length + ' responses in cue...', 
+        3000);
+  }
+
+  createPoll(form) {
+    // Possible refactoring, using call or apply?
+    var checkResponses = function(arr) {
+      var count = arr.length;
+      arr.forEach(function(el, i) {
+        if(el.response === '') count--;
+      });
+      console.log('count: ', count);
+      return count >= 3 ? true : false;
+    };
+    // Possible refactoring, using call or apply?
+    var clearBlankFields = function(arr) {
+      for(var i = arr.length-1; i >= 0; i--) {
+        if(arr[i].response === '') arr.slice(-1,1);
+      };
+      return arr;
+    };
+
+    if(form.$valid && checkResponses(this.answers)) {
+      this.answers = clearBlankFields(this.answers);
+
+      var promise = this.poll.create({
+        owner: this.auth.getCurrentUser()._id,
         question: this.question,
         responses: this.answers
-      }, function(data) {
-        console.log('success', data);
-        this.resetVars();
-        this.toaster
-          .pop('success', 
-            "Poll Created!", 
-            this.pollData.question, 
-            3000);
-      }, function(err) {
-          console.warn('error', err);
-          this.resetVars();
+      }).$promise;
+      promise
+        .then(function(poll) {
           this.toaster
-              .pop('error', 
-                'Poll create failed...', 
-                err, 
-                3000);
-      });
+            .pop('success', 
+              "Poll Created!", 
+              poll.question, 
+              3000);
+          this.resetVars();
+        }.bind(this))
+        .catch(function(err) {
+          this.toaster
+            .pop('error', 
+              'Poll create failed...', 
+              err, 
+              3000);
+        }.bind(this));
     } else {
       this.toaster
         .pop('error',
           'Form is incomplete',
-          'try again!',
+          'sorry!',
           3000);
     }
   }
--- server/api/poll/poll.model.js
+++ server/api/poll/poll.model.js
@@ -5,17 +5,15 @@ import mongoose from 'mongoose';
 var PollSchema = new mongoose.Schema({
 
   owner: {type: mongoose.Schema.Types.ObjectId, required: true},
+	question: {type: String, required: true},
 
-  list: [{
-  	question: {type: String,},
-  	dateCreated: {type: Date, default: Date.now()},
-  	active: {type: Boolean, default: true},
+  dateCreated: {type: Date, default: Date.now()},
+  active: {type: Boolean, default: true},
 
-  	responses: [{
-  		response: {type: String, },
-  		count: {type: Number, default: 0}
-  	}]
-  }]
+	responses: [{
+		response: {type: String},
+		count: {type: Number, default: 0}
+	}]
 
 });
 
